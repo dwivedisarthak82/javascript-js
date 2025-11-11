de1ae68a14fe9de877e61746ac1fdcce2ed536b8
dwivedisarthak82 <srkdwivedi82@gmail.com>
2025-11-11 14:49:40 +0530

    node js modules

---
+++ hitesh sir/4.comparsion.js
@@ -0,0 +1,18 @@
+const log =(msg)=> console.log(msg);
+let arr =[1,3,4,5,6];
+//log(arr);
+arr.push(125);
+log(arr);
+
+// const user={
+//     id:"abc",
+//     name:"john",
+//     class:12,
+//     address:{
+//      street:"ratnagiri",
+//      pincode:462022,
+        
+//     }
+
+// }
+
